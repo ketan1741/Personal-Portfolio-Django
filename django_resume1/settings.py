f9314a4643fc756fd7e18fda0cc5fb4a199bd05c
Ketan Ramaneti <48892592+ketan1741@users.noreply.github.com>
2022-09-03 16:54:05 +0530

    removed secret key

--- django_resume1/settings.py
+++ django_resume1/settings.py
@@ -29,7 +29,7 @@ EMAIL_USE_TLS = True
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
